1423a39576fd7b349a5681f6ef9b029b7efa4e32
kys6879 <kys6879@naver.com>
2019-04-28 12:32:47 +0900

    라우터 수정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { LoginComponent } from './login/login.component'
 import { HomeComponent } from './home/home.component'
 const routes: Routes = [
     // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: '', component: AppComponent, pathMatch: 'full' },
-    { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     // { path: '**', component: PageNotFoundComponent }
 ];
